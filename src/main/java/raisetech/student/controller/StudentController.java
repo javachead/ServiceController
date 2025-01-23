d7f91fb3fb9ec7bcef94c691acc48f74e9807e9c
javachead <tensyokuyoudesu11@gmail.com>
2025-01-23 14:18:45 +0900

    修正：REST仕様へのリファクタリング3

--- src/main/java/raisetech/student/controller/StudentController.java
+++ src/main/java/raisetech/student/controller/StudentController.java
@@ -147,8 +147,4 @@ public class StudentController {
         response.put("message", "学生が削除されました");
         return ResponseEntity.ok(response);
     }
-
-    public Logger getLogger() {
-        return logger;
-    }
 }
