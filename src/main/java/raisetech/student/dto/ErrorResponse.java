fdb1cd989467a366707779987c329028318be190
javachead <tensyokuyoudesu11@gmail.com>
2025-02-06 20:36:11 +0900

    ①不要アノテーションの削除

--- src/main/java/raisetech/student/dto/ErrorResponse.java
+++ src/main/java/raisetech/student/dto/ErrorResponse.java
@@ -1,16 +1,10 @@
 package raisetech.student.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 // Lombokを利用したコンストラクタ生成
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ErrorResponse {
     private String message;
     private HttpStatus httpStatus;
