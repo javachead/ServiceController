e89a5f61e99fdb93d9b5de95e2c84190a232a9ed
javachead <tensyokuyoudesu11@gmail.com>
2025-03-04 23:26:23 +0900

    StudentDetailTestの修正5　コード削除

--- src/test/java/raisetech/student/domain/StudentDetailTest.java
+++ src/test/java/raisetech/student/domain/StudentDetailTest.java
@@ -1,10 +1,7 @@
 package raisetech.student.domain;
 
 import org.junit.jupiter.api.Test;
-import raisetech.student.data.Student;
-import raisetech.student.data.StudentCourse;
 
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,23 +19,5 @@ class StudentDetailTest {
                 .isNotNull()           // nullではないこと
                 .isInstanceOf(List.class) // リストであること
                 .isEmpty();            // 空であること
-    }
-
-
-    /*StudentDetail クラスの equals メソッドが正しく動作するかを検証する*/
-    @Test
-    void オブジェクト内容を全体で比較する() {
-        // 実際のオブジェクト
-        StudentDetail actual = new StudentDetail();
-        actual.setStudent(null); // 初期状態
-        actual.setStudentCourses(Collections.emptyList());
-
-        // 不正なデータでエラーを確かめる例
-        StudentDetail expected = new StudentDetail();
-        expected.setStudent(new Student()); // 違う値を設定
-        expected.setStudentCourses(Collections.singletonList(new StudentCourse()));
-
-        // 期待される結果と異なることを確認（クラスの責務を検証する）
-        assertThat(actual).isNotEqualTo(expected);
     }
 }
